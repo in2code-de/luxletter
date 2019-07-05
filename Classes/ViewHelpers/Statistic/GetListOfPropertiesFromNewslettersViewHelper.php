38894d6be773342389fc4a8b4ab4a5e5bf490eed
Alex Kellner <alexander.kellner@in2code.de>
2019-07-05 19:12:44 +0200

    [TASK] Bring open and click statistic in dashboard to live

---
+++ Classes/ViewHelpers/Statistic/GetListOfPropertiesFromNewslettersViewHelper.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+namespace In2code\Luxletter\ViewHelpers\Statistic;
+
+use In2code\Luxletter\Domain\Model\Newsletter;
+use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
+use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class GetOpenersViewHelper
+ * to get a commaseparated list of openers from newsletters
+ */
+class GetListOfPropertiesFromNewslettersViewHelper extends AbstractViewHelper
+{
+
+    /**
+     * @return void
+     */
+    public function initializeArguments(): void
+    {
+        parent::initializeArguments();
+        $this->registerArgument('newsletters', QueryResultInterface::class, 'Newsletter', true);
+        $this->registerArgument('property', 'string', 'Any concatinated property from a newsletter', true);
+        $this->registerArgument('limit', 'int', 'Show X values', false, 6);
+    }
+
+    /**
+     * @return string
+     */
+    public function render(): string
+    {
+        $values = [];
+        foreach ($this->arguments['newsletters'] as $newsletter) {
+            /** @var Newsletter $newsletter */
+            $values[] = ObjectAccess::getProperty($newsletter, $this->arguments['property']);
+        }
+        return implode(',', $values);
+    }
+}
