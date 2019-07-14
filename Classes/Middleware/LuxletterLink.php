c7f78bd396d48a846c6d1bd497e7eea7f2d46ec1
Alex Kellner <alexander.kellner@in2code.de>
2019-07-14 14:59:26 +0200

    [FEATURE] Add identification of user in lux when opening a link in newsletter

--- Classes/Middleware/LuxletterLink.php
+++ Classes/Middleware/LuxletterLink.php
@@ -2,20 +2,28 @@
 declare(strict_types=1);
 namespace In2code\Luxletter\Middleware;
 
+use In2code\Lux\Domain\Factory\VisitorFactory;
+use In2code\Lux\Domain\Tracker\AttributeTracker;
+use In2code\Lux\Utility\CookieUtility;
 use In2code\Luxletter\Domain\Model\Link;
 use In2code\Luxletter\Domain\Repository\LinkRepository;
 use In2code\Luxletter\Domain\Service\LogService;
+use In2code\Luxletter\Utility\ExtensionUtility;
 use In2code\Luxletter\Utility\ObjectUtility;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
+use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
 use TYPO3\CMS\Core\Http\RedirectResponse;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException;
+use TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException;
 
 /**
  * Class LuxletterLink
+ * to redirect a luxletterlink to its target and track the click before
  */
 class LuxletterLink implements MiddlewareInterface
 {
@@ -24,7 +32,11 @@ class LuxletterLink implements MiddlewareInterface
      * @param ServerRequestInterface $request
      * @param RequestHandlerInterface $handler
      * @return ResponseInterface
+     * @throws ExtensionConfigurationExtensionNotConfiguredException
+     * @throws ExtensionConfigurationPathDoesNotExistException
      * @throws IllegalObjectTypeException
+     * @throws UnknownObjectException
+     * @throws \Doctrine\DBAL\DBALException
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
@@ -32,6 +44,7 @@ class LuxletterLink implements MiddlewareInterface
             $linkRepository = ObjectUtility::getObjectManager()->get(LinkRepository::class);
             /** @var Link $link */
             $link = $linkRepository->findOneByHash($this->getHash());
+            $this->luxIdentification($link);
             if ($link !== null) {
                 $logService = ObjectUtility::getObjectManager()->get(LogService::class);
                 $logService->logLinkOpening($link);
@@ -55,5 +68,35 @@ class LuxletterLink implements MiddlewareInterface
     protected function getHash(): ?string
     {
         return GeneralUtility::_GP('luxletterlink');
+    }
+
+    /**
+     * Identification of user in EXT:lux
+     *
+     * @param Link $link
+     * @return void
+     * @throws ExtensionConfigurationExtensionNotConfiguredException
+     * @throws ExtensionConfigurationPathDoesNotExistException
+     * @throws IllegalObjectTypeException
+     * @throws UnknownObjectException
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Exception
+     */
+    protected function luxIdentification(Link $link): void
+    {
+        if (ExtensionUtility::isLuxAvailable('5.0.0')) {
+            $idCookie = CookieUtility::getLuxId();
+            if ($idCookie === '') {
+                $idCookie = CookieUtility::setLuxId();
+            }
+            $visitorFactory = ObjectUtility::getObjectManager()->get(VisitorFactory::class, $idCookie);
+            $visitor = $visitorFactory->getVisitor();
+            $attributeTracker = ObjectUtility::getObjectManager()->get(
+                AttributeTracker::class,
+                $visitor,
+                AttributeTracker::CONTEXT_LUXLETTERLINK
+            );
+            $attributeTracker->addAttribute('email', $link->getUser()->getEmail());
+        }
     }
 }
---
+++ Classes/Utility/ExtensionUtility.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+namespace In2code\Luxletter\Utility;
+
+use TYPO3\CMS\Core\Package\Exception;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
+/**
+ * Class ExtensionUtility
+ */
+class ExtensionUtility
+{
+
+    /**
+     * @param string $minimumVersion if set check for minimum version like "5.0.0"
+     * @return bool
+     * @throws Exception
+     */
+    public static function isLuxAvailable(string $minimumVersion = ''): bool
+    {
+        if ($minimumVersion === '') {
+            return ExtensionManagementUtility::isLoaded('lux');
+        } else {
+            $versionNumberLux = VersionNumberUtility::convertVersionNumberToInteger(
+                ExtensionManagementUtility::getExtensionVersion('lux')
+            );
+            return $versionNumberLux >= VersionNumberUtility::convertVersionNumberToInteger($minimumVersion);
+        }
+    }
+}
