91c9e4f904738e3a609184c82d3e5f9686323c84
Alex Kellner <alexander.kellner@in2code.de>
2019-11-26 11:18:12 +0100

    [BUGFIX] Increase maximum size of tx_luxletter_domain_model_newsletter.bodytext
    from 64KB to 16MB
    
    Related: https://github.com/in2code-de/luxletter/issues/9

--- ext_tables.sql
+++ ext_tables.sql
@@ -12,7 +12,7 @@ CREATE TABLE tx_luxletter_domain_model_newsletter (
 	subject varchar(255) DEFAULT '' NOT NULL,
 	receiver int(11) DEFAULT '0' NOT NULL,
 	origin varchar(255) DEFAULT '' NOT NULL,
-	bodytext text NOT NULL,
+	bodytext mediumtext NOT NULL,
 	disabled tinyint(4) unsigned DEFAULT '0' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
