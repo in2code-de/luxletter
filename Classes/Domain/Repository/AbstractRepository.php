c60ca158abcdeea2c85976a67399fc1deddb25af
Alex Kellner <alexander.kellner@in2code.de>
2020-03-17 22:18:46 +0100

    [TASK] Small cleanup

--- Classes/Domain/Repository/AbstractRepository.php
+++ Classes/Domain/Repository/AbstractRepository.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace In2code\Luxletter\Domain\Repository;
 
 use In2code\Luxletter\Utility\ObjectUtility;
+use TYPO3\CMS\Extbase\Object\Exception;
 use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
 use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
@@ -33,6 +34,7 @@ abstract class AbstractRepository extends Repository
 
     /**
      * @return void
+     * @throws Exception
      */
     public function persistAll()
     {
