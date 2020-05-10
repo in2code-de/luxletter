cdf0836d6128dfa1fec5429278554243c5815b11
Alex Kellner <alexander.kellner@in2code.de>
2020-05-10 23:04:46 +0200

    [TASK] Release preperations

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'luxletter - TYPO3 Email Marketing Newsletter Tool',
     'description' => 'Free newsletter extension for TYPO3 for an individual email marketing. A lot of analytics and modern concepts. Works with and without EXT:lux.',
     'category' => 'plugin',
-    'version' => '2.2.2',
+    'version' => '2.3.0',
     'author' => 'Alex Kellner',
     'author_email' => 'alexander.kellner@in2code.de',
     'author_company' => 'in2code.de',
@@ -22,7 +22,9 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '9.5.0-10.4.99'
         ],
         'conflicts' => [],
-        'suggests' => [],
+        'suggests' => [
+            'lux' => '0.0.0-0.0.0'
+        ],
     ],
     '_md5_values_when_last_written' => '',
 ];
