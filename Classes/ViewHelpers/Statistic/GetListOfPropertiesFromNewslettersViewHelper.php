in2code-de/luxletter

commit 3face8f468641689bbfaf9a43e2d3b4e478198d1

Remove comma from newsletter title for statistics
Closes: #111

Classes/ViewHelpers/Statistic/GetListOfPropertiesFromNewslettersViewHelper.php
@@ -35,7 +35,7 @@ class GetListOfPropertiesFromNewslettersViewHelper extends AbstractViewHelper
         $values = [];
         foreach ($this->arguments['newsletters'] as $newsletter) {
             /** @var Newsletter $newsletter */
-            $values[] = ObjectAccess::getProperty($newsletter, $this->arguments['property']);
+            $values[] = str_replace(',', '', ObjectAccess::getProperty($newsletter, $this->arguments['property']));
         }
         return implode(',', $values);
     }

commit ab4d9814aaa07986f7ff8754c38f32b370a17078

[BUGFIX] Fix possible regression with 3face8f468641689bbfaf9a43e2d3b4e478198d1
getProperty() can also return an integer value what would then lead to an exception as long as str_replace() requires a string as third parameter

Classes/ViewHelpers/Statistic/GetListOfPropertiesFromNewslettersViewHelper.php
@@ -35,7 +35,11 @@ class GetListOfPropertiesFromNewslettersViewHelper extends AbstractViewHelper
         $values = [];
         foreach ($this->arguments['newsletters'] as $newsletter) {
             /** @var Newsletter $newsletter */
-            $values[] = str_replace(',', '', ObjectAccess::getProperty($newsletter, $this->arguments['property']));
+            $values[] = str_replace(
+                ',',
+                '',
+                (string)ObjectAccess::getProperty($newsletter, $this->arguments['property'])
+            );
         }
         return implode(',', $values);
     }