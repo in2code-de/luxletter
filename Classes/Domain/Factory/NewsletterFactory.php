143ed7c79a539e74c4d272e30b778ba6527341fa
Alexander Kellner <alexander.kellner@in2code.de>
2022-03-02 21:43:21 +0100

    [FEATURE] Multilanguage: Update createNewsletterFromOriginCommand to support newsletters in single languages
    This is a breaking change because, subject is no more required now.

--- Classes/Domain/Factory/NewsletterFactory.php
+++ Classes/Domain/Factory/NewsletterFactory.php
@@ -3,17 +3,19 @@ declare(strict_types = 1);
 namespace In2code\Luxletter\Domain\Factory;
 
 use DateTime;
+use Doctrine\DBAL\Driver\Exception as ExceptionDbalDriver;
 use Exception;
 use In2code\Luxletter\Domain\Model\Configuration;
 use In2code\Luxletter\Domain\Model\Newsletter;
 use In2code\Luxletter\Domain\Repository\ConfigurationRepository;
 use In2code\Luxletter\Domain\Repository\NewsletterRepository;
+use In2code\Luxletter\Domain\Repository\PageRepository;
 use In2code\Luxletter\Domain\Repository\UsergroupRepository;
 use In2code\Luxletter\Domain\Service\Parsing\NewsletterUrl;
 use In2code\Luxletter\Exception\ApiConnectionException;
 use In2code\Luxletter\Exception\InvalidUrlException;
 use In2code\Luxletter\Exception\MisconfigurationException;
-use In2code\Luxletter\Exception\RequestException;
+use In2code\Luxletter\Utility\ConfigurationUtility;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
 use TYPO3\CMS\Core\Exception\SiteNotFoundException;
@@ -30,17 +32,17 @@ use TYPO3\CMS\Extbase\SignalSlot\Exception\InvalidSlotReturnException;
 class NewsletterFactory
 {
     /**
-     * Create a new newsletter
+     * Create a new newsletter and fill the queue
      *
      * @param string $title
-     * @param string $subject
      * @param int $usergroupIdentifier
      * @param int $configurationIdentifier
      * @param string $origin
      * @param int $language
      * @param string $layout
      * @param string $description
-     * @param string $date Date format could be "2022-01-23T00:00"
+     * @param string $date Date format should be "2022-01-23T00:00"
+     * @param string $subject Is only needed if extension runs NOT in multilanguage mode
      * @return Newsletter
      * @throws ExbaseObjectException
      * @throws IllegalObjectTypeException
@@ -51,24 +53,26 @@ class NewsletterFactory
      * @throws MisconfigurationException
      * @throws SiteNotFoundException
      * @throws ApiConnectionException
-     * @throws RequestException
      * @throws ExtensionConfigurationExtensionNotConfiguredException
      * @throws ExtensionConfigurationPathDoesNotExistException
      * @throws Exception
+     * @throws ExceptionDbalDriver
      */
     public function get(
         string $title,
-        string $subject,
         int $usergroupIdentifier,
         int $configurationIdentifier,
         string $origin,
         int $language,
         string $layout,
-        string $description = '',
-        string $date = ''
+        string $description,
+        string $date,
+        string $subject
     ): Newsletter {
-        // Todo: Subject must be parsed
-
+        if (ConfigurationUtility::isMultiLanguageModeActivated()) {
+            $pageRepository = GeneralUtility::makeInstance(PageRepository::class);
+            $subject = $pageRepository->getSubjectFromPageIdentifier((int)$origin, $language);
+        }
         $usergroupRepository = GeneralUtility::makeInstance(UsergroupRepository::class);
         $configurationRepository = GeneralUtility::makeInstance(ConfigurationRepository::class);
         /** @var Configuration $configuration */
