95f65f6b35d59a0c0ae6287efd8de0a2aba1b2c6
Alexander Kellner <alexander.kellner@in2code.de>
2023-07-16 14:56:05 +0200

    [FEATURE] Second step to introduce a asynchronous queue feature with a build queue command
    Added a new command to build up queues

---
+++ Classes/Command/BuildQueueCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+namespace In2code\Luxletter\Command;
+
+use In2code\Luxletter\Domain\Repository\NewsletterRepository;
+use In2code\Luxletter\Domain\Service\QueueService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class BuildQueueCommand extends Command
+{
+    public function configure()
+    {
+        $this->setDescription('Build a queue to a newsletter. Normally used together with "asynchronousQueueStorage"');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $newsletterRepository = GeneralUtility::makeInstance(NewsletterRepository::class);
+        $newsletter = $newsletterRepository->findOneNotQueued();
+
+        if ($newsletter !== null) {
+            $newsletter->setQueued();
+            $newsletterRepository->update($newsletter);
+            $newsletterRepository->persistAll();
+
+            $queueService = GeneralUtility::makeInstance(QueueService::class);
+            $queuedCount = $queueService->addMailReceiversToQueue($newsletter, $newsletter->getLanguage());
+            $output->writeln('Added ' . $queuedCount . ' queue records for newsletter with UID ' . $newsletter->getUid());
+        } else {
+            $output->writeln('No newsletters for new queues found');
+        }
+
+        return parent::SUCCESS;
+    }
+}
