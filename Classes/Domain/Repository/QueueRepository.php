34335f790fb9acb65cff6172b6bc1e12092590b2
Marcellus Siegburg <531939+marcellussiegburg@users.noreply.github.com>
2026-01-29 15:39:17 +0100

    Make SQL queries PostgreSQL compatible

--- Classes/Domain/Repository/QueueRepository.php
+++ Classes/Domain/Repository/QueueRepository.php
@@ -94,7 +94,7 @@ class QueueRepository extends AbstractRepository
         return (int)$queryBuilder
             ->select('uid')
             ->from(Queue::TABLE_NAME)
-            ->where('newsletter=' . $newsletter->getUid() . ' and user=' . $user->getUid())
+            ->where("newsletter=" . (int)$newsletter->getUid() . " and user='" . (int)$user->getUid() . "'")
             ->setMaxResults(1)
             ->executeQuery()
             ->fetchOne() > 0;
